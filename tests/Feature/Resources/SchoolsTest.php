550a36d7ab3f66c53603682886088483ca41600f
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2023-10-21 21:03:16 +0100

    Added RequestMatcher to Schools feature test.

--- tests/Feature/Resources/SchoolsTest.php
+++ tests/Feature/Resources/SchoolsTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Wonde\Tests\Feature\Resources;
 
+use Http\Message\RequestMatcher;
+use Psr\Http\Message\RequestInterface;
 use Wonde\Tests\Feature\TestCase;
 
 class SchoolsTest extends TestCase
@@ -17,7 +19,15 @@ class SchoolsTest extends TestCase
             ),
         );
 
-        $this->mockHttpClient->addResponse($response);
+        $this->mockHttpClient->on(new class() implements RequestMatcher {
+            public function matches(RequestInterface $request): bool
+            {
+                return match ($request->getUri()->getPath()) {
+                    'v1.0/schools/A1930499544' => true,
+                    default => false,
+                };
+            }
+        }, $response);
 
         $response = $this->client->schools->getRaw('A1930499544');
         self::assertEquals(200, $response->getStatusCode());
