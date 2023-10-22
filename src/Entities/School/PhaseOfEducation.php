9a97d6d0a90a74fe228560424165d7423fe61a51
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2023-10-22 12:39:17 +0100

    Additional PhaseOfEducation cases.

--- src/Entities/School/PhaseOfEducation.php
+++ src/Entities/School/PhaseOfEducation.php
@@ -7,6 +7,9 @@ namespace Wonde\Entities\School;
 enum PhaseOfEducation: string
 {
     case NOT_APPLICABLE = 'NOT APPLICABLE';
+    case NOT_APPLICABLE_DEEMED_PRIMARY = 'NOT APPLICABLE DEEMED PRIMARY';
+    case NOT_APPLICABLE_DEEMED_SECONDARY = 'NOT APPLICABLE DEEMED SECONDARY';
+    case NURSERY = 'NURSERY';
     case PRIMARY = 'PRIMARY';
     case SECONDARY = 'SECONDARY';
     case SIXTEEN_PLUS = '16 PLUS';
