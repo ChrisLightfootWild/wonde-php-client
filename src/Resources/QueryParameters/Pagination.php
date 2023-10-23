ef17588a1c60e7929550bc9d5e0004cbe3d7a16b
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2023-10-23 16:42:39 +0100

    Pagination can be cursor based.

--- src/Resources/QueryParameters/Pagination.php
+++ src/Resources/QueryParameters/Pagination.php
@@ -9,14 +9,16 @@ use Wonde\Contracts\Requests\QueryParameter;
 class Pagination implements QueryParameter
 {
     public function __construct(
-        public readonly int $perPage,
+        public readonly ?int $perPage = null,
+        public readonly bool $useCursor = false,
     ) {
     }
 
     public function toQueryString(): array
     {
-        return [
+        return array_filter([
             'per_page' => $this->perPage,
-        ];
+            'cursor' => $this->useCursor,
+        ]);
     }
 }
